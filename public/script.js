75cf4ef6760ab7e0de4ac6cc48d97c96ac44950b
prastutsapkota <prastut.sapkota@gmail.com>
2022-12-07 22:27:49 +1100

    Added database and features

--- public/script.js
+++ public/script.js
@@ -11,12 +11,49 @@ const clickMe = () => {
     return document.getElementById("capital_cities").innerHTML = optext;
 }
 
+const getProjects = () => {
+    $.get('/api/projects',(response) => {
+        if(response.statusCode == 200){
+            addCards(response.data);
+        }
+    })
+}
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+        '</div><div class="card-content">'+'<span class="card-title activator grey-text text-darken-4">'+item.title+
+        '<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+
+        '</a></p></div>'+'<div class="card-reveal">'+'<span class="card-title grey-text text-darken-4">'+item.title+
+        '<i class="material-icons right">close</i></span>'+'<p class="card-text">'+item.desciption+'</p>'+'</div></div></div>';
+        $("#card-section").append(itemToAppend);
+        console.log(itemToAppend);
+    });
+}
+
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.desciption = $('#desciption').val();
+    addProjectToApp(formData);
     console.log("Form Data Submitted", formData);
+}
+
+const addProjectToApp = (project) => {
+    $.ajax(
+        {
+            url: '/api/projects',
+            data: project,
+            type: 'POST',
+            success: (result) => {
+                alert(result.message);
+                location.reload();
+            }
+        }
+    )
 }
 
 $(document).ready(function(){
@@ -24,18 +61,12 @@ $(document).ready(function(){
     $('#clickMeButton').click(()=>{
         clickMe();
     })
+    // addCards(cardList);
     $('#formSubmit').click(()=>{
         submitForm();
     })
-    // getProjects();
+    getProjects();
 
 });
 
 
-// const getProjects = () => {
-//     $.get('/api',(response) => {
-//         if(response.statusCode == 200){
-//             addCards(response.data);
-//         }
-//     })
-// }
